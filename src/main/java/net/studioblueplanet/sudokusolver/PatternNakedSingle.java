487810de32f8cd380b7ac1bd30e824a3bcf87e75
Jorgen <scubajorgen@gmail.com>
2020-12-14 14:20:14 +0100

    Simplified implementation

--- src/main/java/net/studioblueplanet/sudokusolver/PatternNakedSingle.java
+++ src/main/java/net/studioblueplanet/sudokusolver/PatternNakedSingle.java
@@ -15,40 +15,11 @@ public class PatternNakedSingle extends Pattern
 {
     public static int execute(Sudoku sudoku)
     {
-        int dimension;
-        int row;
-        int col;
-        int boxRow;
-        int boxCol;
-        int cellRow;
-        int cellCol;
-        int value;
         int count;
         
-        count=0;
-        dimension=sudoku.getDimension();
-        for (row=0; row<dimension*dimension; row++)
-        {
-            boxRow =sudoku.indexToBox(row);
-            cellRow=sudoku.indexToCell(row);
-            for (col=0; col<dimension*dimension; col++)
-            {
-                boxCol =sudoku.indexToBox(col);
-                cellCol=sudoku.indexToCell(col);
-                if (sudoku.getElementValue(boxRow, boxCol, cellRow, cellCol)==Sudoku.UNDEFINED)
-                {
-                    for(value=1; value<=dimension*dimension; value++)
-                    {
-                        if (sudoku.boxHasValue(boxRow, boxCol, value) || sudoku.rowHasValue(row, value) || sudoku.colHasValue(col, value))
-                        {
-                            sudoku.setCandidate(boxRow, boxCol, cellRow, cellCol, value, false);
-                        }
-                    }
-                }
-            }
-        }
-        
-        // Check if this resulted in naked singles; write them to the cells
+        // simply collapse the elements that contain one candidate (the naked
+        // single). By setting an element value the candidates are updated 
+        // automatically.
         count=sudoku.collapseCandidates();
         return count;
     }
